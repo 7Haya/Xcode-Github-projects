ca943c104b9ba8c1e2eb2cb9abb1b8e332f00045
7Haya <147266627+7Haya@users.noreply.github.com>
2023-10-07 22:51:12 +0000

    Add files via upload

---
+++ water/water/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  water
+//
+//  Created by Haya Fahad Alsabr on 23/03/1445 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+        
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}

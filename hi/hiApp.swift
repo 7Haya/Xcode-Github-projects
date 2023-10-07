25e9f1225745ec2e05c96dd8581cf7a44c06721c
Haya <hayafahad46@gmail.com>
2023-10-07 19:49:36 +0300

    Initial Commit

---
+++ hi/hiApp.swift
@@ -0,0 +1,17 @@
+//
+//  hiApp.swift
+//  hi
+//
+//  Created by Haya Fahad Alsabr on 22/03/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct hiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
